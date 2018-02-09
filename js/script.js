f52b7bdf6995e5fe9eb6fbd8380406e3a5541503
Hiram Cho <you@example.com>
2018-02-08 19:35:41 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,143 @@
+//STEP 1
+//var someMonth;
+//function theMonth() {}
+//const currentMonth;
+//var summerMonth = [];
+//function myLibraryaFunction() {};
+
+//STEP 2
+//var num = 2;
+//var string = 'abcd';
+//var x = true;
+//var y = null;
+
+//STEP 3
+//var add = 4 + 4 + (3 + 1);
+//var math = 4 * 7 % (12 * 4);
+
+////STEP 4
+//var firstName, lastName, address, city, state, zipCode, youAge, referralSource, mayWeContactYou;
+//
+////STEP 5
+//firstName = 'Hiram';
+//city = 'San Diego';
+//zipCode = '92110';
+//
+//////STEP 6
+////var x = console.log(2 + 'some string');
+////var i = console.log(false + 'wacky shoes');
+////var q = console.log(3 + true);
+////
+//////STEP 7
+////function test() {
+////    var fruit = 'apples';
+////    console.log('I like to eat ' + fruit + ' and ' + snacks);
+////    var snacks = 'nutella';
+////}
+////
+//////STEP 8
+////No, string literal is not valid. Need to add '\' to 'I\'m'.
+//
+////STEP 9
+//var empty = null;
+//console.log(empty);
+//var undef;
+//console.log(undef);
+//
+////STEP 10
+//typeof 'my name';
+//typeof 9939;
+//typeof true;
+//typeof someObj ();
+//typeof variable;
+//
+////STEP 11
+//window.alert('Hello ' + 'Big Baller, ' + 'welcome to the Javascript Class!');
+//
+////STEP 12
+//var name = 'Big Baller';
+//window.alert('Hello ' +  name + ', welcome to the Javascript Class!');
+//
+////STEP 13
+//var course = 'JavaScript';
+//window.alert('Hello ' +  name + ', welcome to the ' + course + ' Class!');
+//
+////STEP 14
+//window.alert('Hello Big Baller.' + '\n' + 'Welcome to the ' + course + ' Class!');
+//
+////STEP 15
+//var response = window.prompt('What is your name?');
+//window.alert('Hello ' + response + '.\n' + 'Welcome to the ' + course + ' Class!');
+//
+////STEP 16
+//var userClass = window.prompt('What class are you taking?');
+//window.alert('Hello ' + response + '.\n' + 'Welcome to the ' + userClass + ' Class!');
+//
+////STEP 17
+//var x = 10;
+//var y = 20;
+//console.log(x + y);
+//
+////STEP 18
+//var x = 20;
+//console.log(x += 20);
+//
+////STEP 19
+//var x = 20;
+//console.log(x *= 5);
+//
+////STEP 20
+//var x = 20 % 3; 
+//console.log(x / 1);
+//
+////STEP 21
+//var x = 50;
+//var y = 20;
+//console.log(x > y);
+//console.log(x != y);
+//
+////STEP 22
+//var x = 99;
+//var y = 22;
+//console.log(x == y);
+//console.log(x < y);
+//
+////STEP 23
+//var widget = new Object();
+//console.log(typeof widget);
+//
+////STEP 24
+//console.log(widget instanceof Object);
+//
+////STEP 25
+//console.log(!(widget instanceof Object));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
